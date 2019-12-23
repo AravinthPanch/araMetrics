c3fc8b7673bb77412de97fd22f7e560ed60beb8e
Aravinth Panch <me@aravinth.info>
2019-12-23 13:14:16 +0100

    Add modularity to the code

--- araMetrics.py
+++ araMetrics.py
@@ -7,36 +7,11 @@ from __future__ import print_function
 import datetime
 import pickle
 import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from config import *
-
-def google_api_login():
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    global service
-    service = build('calendar', 'v3', credentials=creds)
+from api import google_api_login
 
 
-def retrieve_cal_events():
+def retrieve_cal_events(service):
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
@@ -55,8 +30,9 @@ def retrieve_cal_events():
 
 
 def main():
-    google_api_login()
-    retrieve_cal_events()
+    service = google_api_login()
+    retrieve_cal_events(service)
+
 
 if __name__ == '__main__':
     main()
