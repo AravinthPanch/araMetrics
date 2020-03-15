c7c2bd7d2b13b1a972b638e2ed8dfa6da26bd6fc
Aravinth Panch <me@aravinth.info>
2020-03-15 09:25:19 +0100

    Update crontab

--- cron.sh
+++ cron.sh
@@ -3,4 +3,8 @@
 # Description:   Automatic deployment of websites on araCloud
 # Author:        Aravinth Panch
 
-cd /home/aravinth/www/arametrics.aravinth.info/html/ && /usr/bin/python araMetrics.py 2>&1 | tee output.log
+# crontab: 0 5 * * * -> at 5AM every day
+# crontab: 0 12 * * * -> at 12PM every day
+# crontab: 0 20 * * * -> at 8PM every day
+
+cd /home/aravinth/www/arametrics.aravinth.info/ && /usr/bin/python araMetrics.py 2>&1 | tee output.log
