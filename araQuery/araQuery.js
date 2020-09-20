d591075863c1bc68bd3aa875bb50ae00a5572f55
Aravinth Panch <me@aravinth.info>
2020-09-20 14:18:05 +0200

    Add functions read from JSON file and save to CSV after mapping the fields

--- araQuery/araQuery.js
+++ araQuery/araQuery.js
@@ -1,19 +1,52 @@
+"use strict";
 // Author        : Aravinth Panch
 // Description   : Map SLACK Users JSON to flat CSV for export"
 
+const fs = require("fs");
+const { convertArrayToCSV } = require("convert-array-to-csv");
 
-var jsonQuery = require('json-query')
+let raw_data_files = ["data/slack-ml-1.json", "data/slack-ml-2.json"];
+const csv_header = [
+  "real_name",
+  "display_name",
+  "title",
+  "username",
+  "email",
+  "image_original",
+];
+let users_array = [];
+let users_csv = [];
 
-var data = {
-  people: [
-    {name: 'Matt', country: 'NZ'},
-    {name: 'Pete', country: 'AU'},
-    {name: 'Mikey', country: 'NZ'}
-  ]
-}
+// Extract from all the files
+raw_data_files.forEach((raw_data_file) => {
+  // Read the json file and get items
+  let raw_data = fs.readFileSync(raw_data_file);
+  let users_json = JSON.parse(raw_data).items;
 
-var res = jsonQuery('people[country=NZ].name', {
-  data: data
+  // Cherypick needed fields
+  users_json.forEach((item) => {
+    let user = [
+      item.profile.real_name,
+      item.profile.display_name,
+      item.profile.title,
+      item.username,
+      item.profile.email,
+      item.profile.image_original,
+    ];
+    users_array.push(user);
+  });
+
+  // Convert the users data to CSV format
+  users_csv = convertArrayToCSV(users_array, {
+    header: csv_header,
+    separator: ",",
+  });
 });
 
-console.log(res.value)
+
+// Save the CSV data to the CSV file
+console.log(users_csv);
+fs.writeFile("data/output.csv", users_csv, function (err) {
+  if (err) return console.log(err);
+  console.log('Done !!!')
+});
