abbd084250157d7035487cafbb2bbeca1d16c73f
Aravinth Panch <me@aravinth.info>
2020-12-30 16:54:50 +0100

    Add functions to get social detail of LI posts

--- arametrics-linkedin/arametrics-linkedin.py
+++ arametrics-linkedin/arametrics-linkedin.py
@@ -5,14 +5,46 @@
 
 import requests
 import pprint
+import csv
+import json
 
 from config.config import *
+posts = []
 
-with requests.session() as s:
-    s.cookies['li_at'] = LI_ACCESS_TOKEN
-    s.cookies["JSESSIONID"] = LI_CSRF_TOKEN
-    s.headers = LI_REQUEST_HEADER
-    s.headers["csrf-token"] = s.cookies["JSESSIONID"].strip('"')
-    response = s.get(LI_REQUEST_URL)
-    response_dict = response.json()
-    pprint.pprint(response_dict)
+with requests.session() as session:
+    session.cookies['li_at'] = LI_ACCESS_TOKEN
+    session.cookies["JSESSIONID"] = LI_CSRF_TOKEN
+    session.headers = LI_REQUEST_HEADER
+    session.headers["csrf-token"] = session.cookies["JSESSIONID"].strip('"')
+    response = session.get(LI_REQUEST_URL).json()
+    response_elements = response['elements']
+    for element in response_elements:
+        SOCIAL_DETAIL_URL = LI_SOCIAL_DETAIL_URL + element['updateMetadata']['urn']
+        print(SOCIAL_DETAIL_URL)
+        social_detail_response = session.get(SOCIAL_DETAIL_URL).json()
+        if social_detail_response.get('status') is None:
+            post = {
+                'URN': "https://www.linkedin.com/feed/update/" + element['updateMetadata']['urn'],
+                'DATE': str(social_detail_response['createdOn']['day']) + '/' + str(social_detail_response['createdOn']['month']) + '/' + str(social_detail_response['createdOn']['year']),
+                'LIKES': social_detail_response['totalSocialActivityCounts']['numLikes'],
+                'VIEWS': social_detail_response['totalSocialActivityCounts']['numViews'],
+                'SHARES': social_detail_response['totalSocialActivityCounts']['numShares'],
+                'COMMENTS': social_detail_response['totalSocialActivityCounts']['numComments'],
+            }
+            posts.append(post)
+
+data_file = open('logs/linkedin-social-detail.csv', 'w')
+csv_writer = csv.writer(data_file)
+count = 0
+for post in posts:
+    if count == 0:
+
+        # Writing headers of CSV file
+        header = post.keys()
+        csv_writer.writerow(header)
+        count += 1
+
+    # Writing data of CSV file
+    csv_writer.writerow(post.values())
+
+data_file.close()
--- arametrics-linkedin/config/config.py
+++ arametrics-linkedin/config/config.py
@@ -1,3 +1,4 @@
 from secrets.keys import *
 LI_REQUEST_HEADER = {"user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",}
-LI_REQUEST_URL = 'https://www.linkedin.com/voyager/api/identity/socialUpdateAnalyticsHeader/urn:li:activity:6749309560533651456'
+LI_SOCIAL_DETAIL_URL = 'https://www.linkedin.com/voyager/api/identity/socialUpdateAnalyticsHeader/'
+LI_REQUEST_URL = 'https://www.linkedin.com/voyager/api/identity/profileUpdatesV2?count=1&includeLongTermHistory=true&moduleKey=member-shares%3Aphone&numComments=0&numLikes=0&profileUrn=urn%3Ali%3Afsd_profile%3AACoAAArvscgBnXp5J2T_ipLQXmXy73eyNWs9zcE&q=memberShareFeed'
