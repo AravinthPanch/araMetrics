fd8770d3e5103817a39e22d9738fd926e399d037
Aravinth Panch <me@aravinth.info>
2020-12-30 22:39:13 +0100

    Update post fetcher

--- arametrics-linkedin/arametrics-linkedin.py
+++ arametrics-linkedin/arametrics-linkedin.py
@@ -10,29 +10,44 @@ import json
 
 from config.config import *
 posts = []
+PAGINATION_TOKEN = ''
 
-with requests.session() as session:
-    session.cookies['li_at'] = LI_ACCESS_TOKEN
-    session.cookies["JSESSIONID"] = LI_CSRF_TOKEN
-    session.headers = LI_REQUEST_HEADER
-    session.headers["csrf-token"] = session.cookies["JSESSIONID"].strip('"')
-    response = session.get(LI_REQUEST_URL).json()
-    response_elements = response['elements']
-    for element in response_elements:
-        SOCIAL_DETAIL_URL = LI_SOCIAL_DETAIL_URL + element['updateMetadata']['urn']
-        print(SOCIAL_DETAIL_URL)
-        social_detail_response = session.get(SOCIAL_DETAIL_URL).json()
-        if social_detail_response.get('status') is None:
-            post = {
-                'URN': "https://www.linkedin.com/feed/update/" + element['updateMetadata']['urn'],
-                'DATE': str(social_detail_response['createdOn']['day']) + '/' + str(social_detail_response['createdOn']['month']) + '/' + str(social_detail_response['createdOn']['year']),
-                'LIKES': social_detail_response['totalSocialActivityCounts']['numLikes'],
-                'VIEWS': social_detail_response['totalSocialActivityCounts']['numViews'],
-                'SHARES': social_detail_response['totalSocialActivityCounts']['numShares'],
-                'COMMENTS': social_detail_response['totalSocialActivityCounts']['numComments'],
-            }
-            posts.append(post)
+# Fetch posts
+for x in range(COUNT_START, COUNT_STOP, COUNT_STEP):
+    if COUNT_START is 0:
+        REQUEST_URL = LI_PROFILE_UPDATES_URL + '&count=' + str(COUNT_STEP)
+    else:
+        REQUEST_URL = LI_PROFILE_UPDATES_URL + '&count=' + \
+            str(COUNT_STEP) + '&start=' + str(COUNT_START) + '&paginationToken=' + str(PAGINATION_TOKEN)
 
+    print(COUNT_START)
+    COUNT_START = COUNT_START + COUNT_STEP
+
+    with requests.session() as session:
+        session.cookies['li_at'] = LI_ACCESS_TOKEN
+        session.cookies["JSESSIONID"] = LI_CSRF_TOKEN
+        session.headers = LI_REQUEST_HEADER
+        session.headers["csrf-token"] = session.cookies["JSESSIONID"].strip('"')
+        response = session.get(REQUEST_URL).json()
+        PAGINATION_TOKEN = response['metadata']['paginationToken']
+        response_elements = response['elements']
+
+        for element in response_elements:
+            SOCIAL_DETAIL_URL = LI_SOCIAL_DETAIL_URL + element['updateMetadata']['urn']
+            print(SOCIAL_DETAIL_URL)
+            social_detail_response = session.get(SOCIAL_DETAIL_URL).json()
+            if social_detail_response.get('status') is None:
+                post = {
+                    'URN': "https://www.linkedin.com/feed/update/" + element['updateMetadata']['urn'],
+                    'DATE': str(social_detail_response['createdOn']['day']) + '/' + str(social_detail_response['createdOn']['month']) + '/' + str(social_detail_response['createdOn']['year']),
+                    'LIKES': social_detail_response['totalSocialActivityCounts']['numLikes'],
+                    'VIEWS': social_detail_response['totalSocialActivityCounts']['numViews'],
+                    'SHARES': social_detail_response['totalSocialActivityCounts']['numShares'],
+                    'COMMENTS': social_detail_response['totalSocialActivityCounts']['numComments'],
+                }
+                posts.append(post)
+
+# Save JSON as CSV
 data_file = open('logs/linkedin-social-detail.csv', 'w')
 csv_writer = csv.writer(data_file)
 count = 0
--- arametrics-linkedin/config/config.py
+++ arametrics-linkedin/config/config.py
@@ -1,4 +1,7 @@
 from secrets.keys import *
 LI_REQUEST_HEADER = {"user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",}
 LI_SOCIAL_DETAIL_URL = 'https://www.linkedin.com/voyager/api/identity/socialUpdateAnalyticsHeader/'
-LI_REQUEST_URL = 'https://www.linkedin.com/voyager/api/identity/profileUpdatesV2?count=1&includeLongTermHistory=true&moduleKey=member-shares%3Aphone&numComments=0&numLikes=0&profileUrn=urn%3Ali%3Afsd_profile%3AACoAAArvscgBnXp5J2T_ipLQXmXy73eyNWs9zcE&q=memberShareFeed'
+LI_PROFILE_UPDATES_URL = 'https://www.linkedin.com/voyager/api/identity/profileUpdatesV2?includeLongTermHistory=true&moduleKey=member-shares%3Aphone&profileUrn=urn%3Ali%3Afsd_profile%3AACoAAArvscgBnXp5J2T_ipLQXmXy73eyNWs9zcE&q=memberShareFeed'
+COUNT_STEP = 10
+COUNT_START = 0
+COUNT_STOP = 300
